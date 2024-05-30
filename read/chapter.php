4e65a31b03e9fb0eb1ab826650e7857de78de301
naufal453 <testbench58@gmail.com>
2024-05-30 09:57:48 +0700

    Update chapter.php

--- read/chapter.php
+++ read/chapter.php
@@ -250,10 +250,6 @@ $result = $stmt->get_result();
                 echo "<div class='comment'>";
                 echo "<strong>" . htmlspecialchars($row_comments['username']) . ":</strong> " . htmlspecialchars($row_comments['comment_text']) . "<br>";
                 echo "<small>Diposting pada: " . htmlspecialchars($row_comments['created_at']) . "</small>";
-                echo "<form action='report_comment_handler.php' method='post' style='display:inline;'>
-                <input type='hidden' name='comment_id' value='" . htmlspecialchars($row_comments['comment_id']) . "'>
-                <button type='submit'>Report</button>
-                </form>";
                 echo "</div>";
             }
         } else {
